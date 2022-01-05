d93cf617c73f8bb58c72afc19f385bfaec3e745f
sanket mane <sanketm@techved.com>
2022-01-05 14:42:21 +0530

    initial commit

---
+++ example/data.js
@@ -0,0 +1,52 @@
+var ganttData = [
+	{
+		id: 1, name: "SMITH", series: [
+			{ name: "USGI", start: new Date(2022,00,01), end: new Date(2022,00,03), color: '#000', time: '5:30'},
+			{ name: "ICICI Video KYC", start: new Date(2022,00,02), end: new Date(2022,00,05), color: "#f0f0f0" },
+			{ name: "USGI", start: new Date(2022,00,25), end: new Date(2022,00,27), color: '#000', time: '5:30'},			
+		]
+	}, 
+	{
+		id: 2, name: "ALLEN", series: [
+			{ name: "Kotak Security", start: new Date(2022,00,05), end: new Date(2022,00,20) },
+			{ name: "WCT", start: new Date(2022,00,06), end: new Date(2022,00,17), color: "#f0f0f0" },
+			{ name: "Axis POC", start: new Date(2022,00,06), end: new Date(2022,00,17), color: "#e0e0e0" }
+		]
+	}, 
+	{
+		id: 3, name: "WARD", series: [
+			{ name: "Ninja Timesheet", start: new Date(2022,00,11), end: new Date(2022,01,03) },
+			{ name: "Bajaj Allianz", start: new Date(2022,00,15), end: new Date(2022,01,03), color: "#F90" }
+		]
+	}, 
+	{
+		id: 4, name: "JONES", series: [
+			{ name: "SBIL", start: new Date(2022,01,01), end: new Date(2022,01,03) },
+			{ name: "TATA AIA", start: new Date(2022,01,01), end: new Date(2022,01,05), color: "#f0f0f0" }
+		]
+	},
+	{
+		id: 5, name: "MARTIN", series: [
+			{ name: "USGI", start: new Date(2022,02,01), end: new Date(2022,03,20) },
+			{ name: "ThermoFisher", start: new Date(2022,02,01), end: new Date(2022,03,26), color: "#f0f0f0" }
+		]
+	}, 
+	{
+		id: 6, name: "BLAKE", series: [
+			{ name: "HDFC Life", start: new Date(2022,00,05), end: new Date(2022,00,20) },
+			{ name: "PolicyBazar", start: new Date(2022,00,06), end: new Date(2022,00,17), color: "#f0f0f0" },
+			{ name: "Zerodha", start: new Date(2022,00,06), end: new Date(2022,00,20), color: "#e0e0e0" }
+		]
+	}, 
+	{
+		id: 7, name: "CLARK", series: [
+			{ name: "Hindustan Uniliver", start: new Date(2022,00,11), end: new Date(2022,01,03) }
+		]
+	}, 
+	{
+		id: 8, name: "SCOTT", series: [
+			{ name: "TATA Captial", start: new Date(2022,01,01), end: new Date(2022,01,03) },
+			{ name: "Bajaj 360 Web App", start: new Date(2022,01,01), end: new Date(2022,01,05), color: "#f0f0f0" }
+		]
+	}
+];

5735e1b596ae7c241cb554c5ffb88b97f46ebf75
sanket mane <sanketm@techved.com>
2022-02-02 19:28:11 +0530

    pushing latest changes

--- example/data.js
+++ example/data.js
@@ -1,50 +1,50 @@
 var ganttData = [
 	{
-		id: 1, name: "SMITH", series: [
+		id: 1, name: "SMITH", departmentName: "UI development", series: [
 			{ name: "USGI", start: new Date(2022,00,01), end: new Date(2022,00,03), color: '#000'},
 			{ name: "ICICI Video KYC", start: new Date(2022,00,02), end: new Date(2022,00,05), color: "#f0f0f0" },
 			{ name: "USGI", start: new Date(2022,00,25), end: new Date(2022,00,27), color: '#000'},			
 		]
 	}, 
 	{
-		id: 2, name: "ALLEN", series: [
+		id: 2, name: "ALLEN", departmentName: "UI/UX Designer", series: [
 			{ name: "Kotak Security", start: new Date(2022,00,05), end: new Date(2022,00,20) },
 			{ name: "WCT", start: new Date(2022,00,06), end: new Date(2022,00,17), color: "#f0f0f0" },
 			{ name: "Axis POC", start: new Date(2022,00,06), end: new Date(2022,00,17), color: "#e0e0e0" }
 		]
 	}, 
 	{
-		id: 3, name: "WARD", series: [
+		id: 3, name: "WARD", departmentName: "Buisness Analyst", series: [
 			{ name: "Ninja Timesheet", start: new Date(2022,00,11), end: new Date(2022,01,03) },
 			{ name: "Bajaj Allianz", start: new Date(2022,00,15), end: new Date(2022,01,03), color: "#F90" }
 		]
 	}, 
 	{
-		id: 4, name: "JONES", series: [
+		id: 4, name: "JONES", departmentName: "Project Manager", series: [
 			{ name: "SBIL", start: new Date(2022,01,01), end: new Date(2022,01,03) },
 			{ name: "TATA AIA", start: new Date(2022,01,01), end: new Date(2022,01,05), color: "#f0f0f0" }
 		]
 	},
 	{
-		id: 5, name: "MARTIN", series: [
+		id: 5, name: "MARTIN", departmentName: "Content", series: [
 			{ name: "USGI", start: new Date(2022,02,01), end: new Date(2022,03,20) },
 			{ name: "ThermoFisher", start: new Date(2022,02,01), end: new Date(2022,03,26), color: "#f0f0f0" }
 		]
 	}, 
 	{
-		id: 6, name: "BLAKE", series: [
+		id: 6, name: "BLAKE", departmentName: "Backend Development", series: [
 			{ name: "HDFC Life", start: new Date(2022,00,05), end: new Date(2022,00,20) },
 			{ name: "PolicyBazar", start: new Date(2022,00,06), end: new Date(2022,00,17), color: "#f0f0f0" },
 			{ name: "Zerodha", start: new Date(2022,00,06), end: new Date(2022,00,20), color: "#e0e0e0" }
 		]
 	}, 
 	{
-		id: 7, name: "CLARK", series: [
+		id: 7, name: "CLARK", departmentName: "UI Developer", series: [
 			{ name: "Hindustan Uniliver", start: new Date(2022,00,11), end: new Date(2022,01,03) }
 		]
 	}, 
 	{
-		id: 8, name: "SCOTT", series: [
+		id: 8, name: "SCOTT", departmentName: "Marketing", series: [
 			{ name: "TATA Captial", start: new Date(2022,01,01), end: new Date(2022,01,03) },
 			{ name: "Bajaj 360 Web App", start: new Date(2022,01,01), end: new Date(2022,01,05), color: "#f0f0f0" }
 		]
